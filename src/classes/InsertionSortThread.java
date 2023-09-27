33219cd933478736444c74759bc14e4cb1048830
Saveliy Komlenok <skomlenok363@gmail.com>
2023-09-27 23:09:40 +0300

    add insertion sort thread

---
+++ src/classes/InsertionSortThread.java
@@ -0,0 +1,36 @@
+package classes;
+
+import java.util.Arrays;
+
+public class InsertionSortThread extends Thread{
+    int[] array;
+    public InsertionSortThread (int[] array, String threadName){
+        this.array = array;
+        Thread thread = new Thread(this, threadName);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Start -> " + Thread.currentThread().getName());
+        System.out.println("Array for insertion sort" + Arrays.toString(this.array));
+        System.out.println("Array sorted by insertion sort" + Arrays.toString(insertionSort(this.array)));
+        System.out.println("End -> " + Thread.currentThread().getName());
+    }
+
+    public int[] insertionSort(int[] array){
+        for (int left = 0; left < array.length; left++) {
+            int number = array[left];
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                if (number < array[i]) {
+                    array[i + 1] = array[i];
+                } else {
+                    break;
+                }
+            }
+            array[i + 1] = number;
+        }
+        return array;
+    }
+}
--- src/classes/Main.java
+++ src/classes/Main.java
@@ -7,10 +7,12 @@ public class Main {
         ArrayFactory arrayFactory = new ArrayFactory();
         BubbleSortThread bubbleSortThread = new BubbleSortThread(arrayFactory.createArray(10), "Thread1:(Bubble sort)");
         SelectionSortThread selectionSortThread = new SelectionSortThread(arrayFactory.createArray(10), "Thread2:(Selection sort)");
+        InsertionSortThread insertionSortThread = new InsertionSortThread(arrayFactory.createArray(10), "Thread3:(Insertion sort)");
 
         try {
             bubbleSortThread.join();
             selectionSortThread.join();
+            insertionSortThread.join();
         } catch (InterruptedException exception){
             System.out.println("Error: " + exception.getMessage());
         }
