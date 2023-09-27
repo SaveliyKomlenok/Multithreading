5e071bd8f932127e02120acc308f72032d672732
Saveliy Komlenok <skomlenok363@gmail.com>
2023-09-27 23:29:42 +0300

    add Shell sort thread

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -9,12 +9,14 @@ public class Main {
         SelectionSortThread selectionSortThread = new SelectionSortThread(arrayFactory.createArray(10), "Thread2:(Selection sort)");
         InsertionSortThread insertionSortThread = new InsertionSortThread(arrayFactory.createArray(10), "Thread3:(Insertion sort)");
         ShuttleSortThread shuttleSortThread = new ShuttleSortThread(arrayFactory.createArray(10), "Thread4:(Shuttle sort)");
+        ShellSortThread shellSortThread = new ShellSortThread(arrayFactory.createArray(10), "Thread5:(Shell sort)");
 
         try {
             bubbleSortThread.join();
             selectionSortThread.join();
             insertionSortThread.join();
             shuttleSortThread.join();
+            shellSortThread.join();
         } catch (InterruptedException exception){
             System.out.println("Error: " + exception.getMessage());
         }
